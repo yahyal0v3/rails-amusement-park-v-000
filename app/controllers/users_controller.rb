b3349909d17dc87b0104a6f222e109cba8f8bd7e
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-03-23 18:15:13 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users
   # GET /users.json
-  def index
-    @users = User.all
-  end
-
+  
   # GET /users/1
   # GET /users/1.json
   def show
